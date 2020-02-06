f4a1193900b7516eb6a1c2a4706587ffce5158f7
chaitnya1997 <60176632+chaitnya1997@users.noreply.github.com>
2020-02-06 09:43:52 +0530

    Add files via upload

---
+++ src/main/java/sample/war/App.java
@@ -0,0 +1,13 @@
+package sample.war;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "HELLOW CHAITANYA!" );
+    }
+}
